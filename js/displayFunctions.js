b9f4711c8406761cf9fead9c9f17a737e97848e8
Paraspreet Atwal <paraspreetatwal@Paraspreets-MacBook-Pro.local>
2023-11-16 17:15:43 -0700

    Created a function that loads the basic song search form using JS. I have also loaded all of the genre names as well as artists onto the form in their respective selects. Need to create Events still.

--- js/displayFunctions.js
+++ js/displayFunctions.js
@@ -1,4 +1,5 @@
 // columns objects has a list of the following  {header: str, type: str, values: array/list, valueFunction: func, prefix: str, sufix: str}
+
 function listData(data, columns, listName) {
     let container = document.createElement('div');
     container.id = listName;
@@ -36,25 +37,20 @@ function listData(data, columns, listName) {
     }
 }
 
-function loadArtists () {
+function loadArtists() {
 
     const artists = artistsArr;
 
     //test
 
-    for(artist of artists)
-    {
-        console.log(artist["id"]);
-    }
 
 }
 
-function loadGenres () {
+function loadGenres() {
 
     const genres = genresArr;
 
-    for (genre of genres)
-    {
+    for (genre of genres) {
         console.log(genre["id"]);
     }
 
@@ -64,8 +60,154 @@ function loadSampleSongs() {
 
     const sampleSongs = sampleSongsArr;
 
-    for (song of sampleSongs) 
-    {
+    for (song of sampleSongs) {
         console.log(song["song_id"]);
     }
 }
+
+function songSearchForm() {
+
+    //This function loads the form via js. Will be adding events later on
+
+
+    // loadArtists();
+
+    const artists = artistsArr;
+    const genres = genresArr;
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        const form = document.querySelector("form");
+
+        //Section 1 Song name
+
+        const radioOne = document.createElement("input");
+        radioOne.setAttribute('id', 't');
+        radioOne.setAttribute('type', 'radio');
+        radioOne.setAttribute('name', 'typeSelection')
+
+        form.appendChild(radioOne);
+
+        const labelOne = document.createElement("label");
+        labelOne.setAttribute('for', 'title');
+        labelOne.textContent = "Title";
+
+        form.appendChild(labelOne);
+
+        const inputOne = document.createElement("input");
+        inputOne.setAttribute('type', 'text');
+        inputOne.setAttribute('id', 'title');
+
+        form.appendChild(inputOne);
+
+        // Section 2 Artists
+
+        const radioTwo = document.createElement("input");
+        radioTwo.setAttribute('id', 'a');
+        radioTwo.setAttribute('type', 'radio');
+        radioTwo.setAttribute('name', 'typeSelection')
+
+        form.appendChild(radioTwo);
+
+        const labelTwo = document.createElement("label");
+        labelTwo.setAttribute('for', 'title');
+        labelTwo.textContent = "Artist";
+
+        form.appendChild(labelTwo);
+
+        const selectOne = document.createElement("select");
+        selectOne.setAttribute('id', 'selectOne');
+
+
+        //loading artist names into the select via option tags=
+
+
+        const blankOption1 = document.createElement("option");
+
+        selectOne.appendChild(blankOption1);
+
+        for (artist of artists) {
+
+            const option = document.createElement("option");
+
+            option.setAttribute("value", artist['id']);
+
+            option.textContent = artist['name'];
+
+            selectOne.appendChild(option);
+
+        }
+
+        inputOne.setAttribute('id', 'title');
+
+        form.appendChild(selectOne);
+
+        // Section 3 Genres
+
+        const radioThree = document.createElement("input");
+        radioThree.setAttribute('id', 'g');
+        radioThree.setAttribute('type', 'radio');
+        radioThree.setAttribute('name', 'typeSelection')
+
+        form.appendChild(radioThree);
+
+
+        const labelThree = document.createElement("label");
+        labelThree.setAttribute('for', 'title');
+        labelThree.textContent = "Genre";
+
+        form.appendChild(labelThree);
+
+        const selectTwo = document.createElement("select");
+        selectTwo.setAttribute('id', 'selectTwo');
+
+        const blankOption2 = document.createElement("option");
+
+        selectTwo.appendChild(blankOption2);
+
+        for (genre of genres) {
+
+            const option = document.createElement("option");
+
+            option.setAttribute("value", genre['id']);
+
+            option.textContent = genre['name'];
+
+            selectTwo.appendChild(option);
+
+        }
+
+        form.appendChild(selectTwo);
+
+
+        //clear button
+
+        const clearButton = document.createElement("button");
+        clearButton.textContent = "Clear";
+
+        clearButton.setAttribute("id", "clearBtn");
+        clearButton.setAttribute('type', 'reset');
+
+        form.appendChild(clearButton);
+
+
+        //filter button
+
+        const filterButton = document.createElement("button");
+        filterButton.textContent = "Filter";
+
+        filterButton.setAttribute("id", "filterBtn");
+
+        form.appendChild(filterButton);
+
+
+        //Add functionality for this
+
+
+
+    });
+
+
+}
+
+songSearchForm();
