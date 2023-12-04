84955f87ad348b671e0fd8d15be8e75644b44bbc
N473D <ndebl589@mtroyal.ca>
2023-12-04 14:52:31 -0700

    pushing early stages of list rework and fetch local save data

--- js/animations.js
+++ js/animations.js
@@ -1,22 +1,11 @@
-function animateLists(closed, listContainer, listFotamt) {
-    let numColumns = listFotamt.length - 1;
-    if(closed){
-        // console.log("opening");
-        // console.log(numColumns);
-        // console.log(listContainer.childNodes);
-        console.log((listContainer.childNodes.length / numColumns));
-        listContainer.style.height = ((listContainer.childNodes.length / (numColumns -1)) * 50) + "px";
-        listContainer.style.transitionDuration = ((listContainer.childNodes.length / (numColumns -1)) * 50) + "ms";
-        for (let i = 0; i < listContainer.childNodes.length; i += numColumns) {
-            // console.log("row" + Math.floor(i/numColumns));
-            setTimeout(() => {
-                for (let i1 = 0; i1 < numColumns; i1++) {   
-                    // console.log(listContainer.childNodes[i1]);
-                    if(listContainer.childNodes[i + i1].classList.contains("hidden")){
-                        listContainer.childNodes[i + i1].classList.toggle("hidden");
-                    }
-                }
-            }, (i * 10));
-        }
+function animateLists(closed, listContainer) {
+    
+    if (closed) {
+        console.log("opening");
+        setTimeout(() => {
+            if (x.classList.contains("hidden")) {
+                x.classList.toggle("hidden");
+            }
+        }, time);
     }
 }
--- js/dataProcessing.js
+++ js/dataProcessing.js
@@ -1,8 +1,83 @@
+const songsListFilter = [
+    { header: "Title", type: 'p', values: ["title"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: ""},
+    { header: "Artist", type: 'p', values: ["artist", "name"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: [], prefix: "", sufix: ""},
+    { header: "Year", type: 'p', values: ["year"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: ""},
+    { header: "Genre", type: 'p', values: ["genre", "name"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: [], prefix: "", sufix: ""},
+    { header: "Popularity", type: 'p', values: ["details", "popularity"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: [], prefix: "", sufix: ""},
+    { header: "", type: 'button', values: ["song_id"], valueFunction: (obj, values) => obj[values[0]], classList: ['fav-button'], prefix: "", sufix: ""} // favorites column
+];
+
+const genresListFilter = [
+    { header: "Genre", type: 'p', values: ["name"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: "", spacing: "1" }
+];
+
+const artistsListFilter = [
+    { header: "Artist", type: 'p', values: ["name"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: "", spacing: "1" },
+    { header: "Type", type: 'p', values: ["type"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: "", spacing: "1" }
+];
 let songs = [];
-fetch(api)
-    .then(response => response.json())
-    .then(data => {
-        songs = [...data];
-        console.log(data);
-    })
-    .catch(error => console.error(error));
+
+function songRetrival () {
+    if (localStorage.getItem("songs") === null) {
+        console.log("fetching");
+        fetch(api)
+            .then(response => response.json())
+            .then(data => {
+                songs = listData([...data], songsListFilter, "search-songs-list", ['song-list-format']);
+                localStorage.setItem("songs", songs);
+                console.log(data);
+            })
+            .catch(error => console.error(error));
+    } else {
+        console.log("retrieving");
+        songs = localStorage.getItem("songs");
+    }
+}
+
+function listData(data, columns, listName, extraclasses = []) {
+    // console.log(data);
+    let container = document.createElement('ul');
+    container.id = listName;
+    container.style.backgroundColor = "red"
+    container.classList.add("data-list");
+    container.appendChild(generateListHeader(columns));
+    for (const obj of data) {
+        container.appendChild(generateListRow(obj, columns));
+    }
+    extraclasses.forEach((c) => {container.classList.add(c)});
+    return container;
+}
+
+function generateListHeader(columns){
+    let headers = document.createElement('li');
+    headers.classList.add("list-headers");
+    for (let column of columns) {
+        let header = document.createElement('button');
+        header.textContent = column.header;
+        headers.appendChild(header);
+    }
+    return headers;
+}
+
+function generateListRow(obj, columns) {
+    let row = document.createElement('li');
+    row.classList.add('table-row');
+    row.id = obj.song_id;
+    for (let column of columns) {
+        let value = document.createElement(column['type']);
+        column['classList'].forEach(c => {value.classList.add(c);});
+        switch (column['type']) {
+            case 'p':
+                value.textContent = column.valueFunction(obj, column.values);
+                break;
+            case 'button':
+                value.textContent = column.valueFunction(obj, column.values);
+                break;
+            default:
+                console.warn('Missing/Incorrect List Column Type');
+                break;
+        }
+        row.appendChild(value);
+    }
+    return row;
+}
