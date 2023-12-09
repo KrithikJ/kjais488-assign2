6924f22b618a22acba4b1d1a37052b8fa2e9f5c9
N DeBliek <ndebl589@mtroyal.ca>
2023-12-08 21:45:56 -0700

    broken version for pulling other changes

--- js/dataProcessing.js
+++ js/dataProcessing.js
@@ -1,9 +1,9 @@
 const songsListFilter = [
-    { header: "Title", type: 'p', values: ["title"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: "" },
-    { header: "Artist", type: 'p', values: ["artist", "name"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: [], prefix: "", sufix: "" },
-    { header: "Year", type: 'p', values: ["year"], valueFunction: (obj, values) => obj[values[0]], classList: [], prefix: "", sufix: "" },
-    { header: "Genre", type: 'p', values: ["genre", "name"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: [], prefix: "", sufix: "" },
-    { header: "Popularity", type: 'p', values: ["details", "popularity"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: [], prefix: "", sufix: "" },
+    { header: "Title", type: 'p', values: ["title"], valueFunction: (obj, values) => obj[values[0]], classList: ['title'], prefix: "", sufix: "" },
+    { header: "Artist", type: 'p', values: ["artist", "name"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: ['artist'], prefix: "", sufix: "" },
+    { header: "Year", type: 'p', values: ["year"], valueFunction: (obj, values) => obj[values[0]], classList: ['year'], prefix: "", sufix: "" },
+    { header: "Genre", type: 'p', values: ["genre", "name"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: ['genre'], prefix: "", sufix: "" },
+    { header: "Popularity", type: 'p', values: ["details", "popularity"], valueFunction: (obj, values) => obj[values[0]][values[1]], classList: ['popularity'], prefix: "", sufix: "" },
     { header: "", type: 'button', values: ["song_id"], valueFunction: (obj, values) => obj[values[0]], classList: ['fav-button'], prefix: "", sufix: "" } // favorites column
 ];
 
@@ -41,38 +41,79 @@ function songRetrival() {
         console.log("retrieving");
         rawSongs = JSON.parse(localStorage.getItem("songs"));
         [...rawSongs].forEach((s) => {
-                songs.push(generateListRow(s, songsListFilter));
-            })
-            // console.log(songs);
+            songs.push(generateListRow(s, songsListFilter));
+        })
+        // console.log(songs);
     }
+    let canvasElement = document.createElement("canvas");
+    canvasElement.id = "test";
+    document.querySelector("body").appendChild(canvasElement);
+    generateSongRadar(1168, canvasElement);
     loadFavs();
     // listData(songs, songsListFilter, 'all-songs', ['song-list-format']);
 }
 
-function loadFavs(){
-    if(localStorage.getItem("favs") === null){
+function loadFavs() {
+    if (localStorage.getItem("favs") === null) {
         localStorage.setItem("favs", JSON.stringify([]));
     } else {
         favs = JSON.parse(localStorage.getItem("favs"));
     }
     favs.forEach(id => {
         songs.forEach(s => {
-            if(s.id == id && s.hasClass("favs")){
+            if (s.id == id && s.hasClass("favs")) {
                 id.classList.add("favs");
             }
         });
     });
-}  
+}
 
 function saveFavs() {
-    if(localStorage.getItem("favs") === null){
+    if (localStorage.getItem("favs") === null) {
         localStorage.setItem("favs", JSON.stringify([]));
     } else {
         localStorage.setItem("favs", JSON.stringify(favs));
     }
 }
 
-function listData(data, columns, listName, extraclasses = []) {
+function generateSongRadar(id, canvas) {
+    let song = rawSongs.find((s) => s.song_id == id);
+    console.log("test");
+    new Chart(canvas, {
+        type: 'radar',
+        data: {
+            labels: ['Acousticness', 'Danceability', 'Energy', 'Liveness', 'Speechiness', 'Valence'],
+            datasets: [{
+                label: song.title,
+                data: [song['analytics']['acousticness'], song['analytics']['danceability'], song['analytics']['energy'], song['analytics']['liveness'], song['analytics']['speechiness'], song['analytics']['valence']],
+                fill: true,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(255, 99, 132)'
+            }]
+        },
+        options: {
+            elements: {
+                line: {
+                    borderWidth: 3
+                }
+            }
+        }
+    });
+}
+
+// Title s => s.querySelector('.title p').textContent.includes(searchquery))
+// year s => s.querySelector('.year p').textContent > #
+
+function songsLimiter(filtering, sorting) {
+    let songsFiltered = songs.filter(filtering(s));
+    return songsFiltered.sort(sorting(s));
+}
+
+function listData(data, columns, listName, parentNodeQuery = "body", extraclasses = []) {
     // console.log(data);
     let container = document.createElement('table');;
     container.id = listName;
@@ -87,7 +128,7 @@ function listData(data, columns, listName, extraclasses = []) {
     if (document.querySelector("#" + listName) !== null) {
         container = document.querySelector("#" + listName);
     } else {
-        return container;
+        document.querySelector(parentNodeQuery).appendChild(container);
     }
 }
 
@@ -110,7 +151,6 @@ function generateListRow(obj, columns) {
     row.id = obj.song_id;
     for (let column of columns) {
         let value = document.createElement(column['type']);
-        column['classList'].forEach(c => { value.classList.add(c); });
         switch (column['type']) {
             case 'p':
                 value.textContent = column.valueFunction(obj, column.values);
@@ -122,9 +162,10 @@ function generateListRow(obj, columns) {
                 console.warn('Missing/Incorrect List Column Type');
                 break;
         }
-        let c = document.createElement('td');
-        c.appendChild(value);
-        row.appendChild(c);
+        let cell = document.createElement('td');
+        cell.appendChild(value);
+        column['classList'].forEach(c => { cell.classList.add(c); });
+        row.appendChild(cell);
     }
     return row;
 }
